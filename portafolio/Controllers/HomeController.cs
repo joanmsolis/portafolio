fa573ef21bcd997d3653e6d1c47464ee0c4c2d9a
Joan M. Solis Lugo <Joan.Solis@caasd.gob.do>
2023-01-12 16:41:08 -0400

    segunda parte del curso de MVC la sessin crear portafolio

--- portafolio/Controllers/HomeController.cs
+++ portafolio/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using portafolio.Models;
+using portafolio.Servicios;
 using System.Diagnostics;
 
 namespace portafolio.Controllers
@@ -7,27 +8,27 @@ namespace portafolio.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly RepositorioProyectos repositorioProyectos;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, RepositorioProyectos repositorioProyectos)
         {
             _logger = logger;
+            this.repositorioProyectos = repositorioProyectos;
         }
 
         public IActionResult Index()
         {
-            var persona = new Persona() {
-                Nombre = " joan manuel solis Lugo",
-                Edad = 99, 
-                Estados = " republica Dominicana."
-
-        };
-            return View(persona);
+            ViewBag.nombre = "Joan Manuel Solis";
+            ViewBag.edad = 25;
+            var proyectos = repositorioProyectos.ObtenerProyactos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
+            return View(modelo);
         }
-
         public IActionResult Privacy()
         {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,40 @@
+﻿using portafolio.Models;
+
+namespace portafolio.Servicios
+{
+    public class RepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyactos()
+        {
+            return new List<Proyecto>() {new Proyecto
+            {
+               Titulo = "Amazon",
+               Descripcion = "E-Commerce ralizado en ASP.NET core",
+               Link = "https://amazon.com",
+               ImagenURL = "/img/amazon.png"
+            },
+                  new Proyecto
+            {
+               Titulo = "New york Time",
+               Descripcion = "Pagina de noticia en react",
+               Link = "https://nytime.com",
+               ImagenURL = "/img/newyork.png"
+            },
+                   new Proyecto
+            {
+               Titulo = "steam",
+               Descripcion = "tiendas en linea para comprar videos juegos",
+               Link = "https://store.steampowerd.com",
+               ImagenURL = "/img/steam.jpg"
+            },
+                    new Proyecto
+            {
+               Titulo = "Reddit",
+               Descripcion = "REd sociales para compartir en comunidades",
+               Link = "https://reddit.com",
+               ImagenURL = "/img/reddit.png"
+            },
+        };
+        }
+    }
+}
