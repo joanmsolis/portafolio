ea78c103552b78c1ed75d61dc4d69646e374e52d
Joan M. Solis Lugo <Joan.Solis@caasd.gob.do>
2023-02-06 16:16:34 -0400

    en este bran trabaje los fomulario

---
+++ portafolio/Servicios/ServicioUnico.cs
@@ -0,0 +1,31 @@
+﻿namespace portafolio.Servicios
+{
+    public class ServicioUnico
+    {
+        public ServicioUnico()
+        {
+            ObtenerGuid = Guid.NewGuid();
+
+        }
+        public Guid ObtenerGuid { get; private set; }
+    }
+
+    public class ServicioDeLimitado
+    { 
+        public ServicioDeLimitado()
+        {
+            ObtenerGuid = Guid.NewGuid();
+
+        }
+        public Guid ObtenerGuid { get; private set; }
+    }
+    public class ServicioTransitorio
+    {
+        public ServicioTransitorio()
+        {
+            ObtenerGuid = Guid.NewGuid();
+
+        }
+        public Guid ObtenerGuid { get; private set; }
+    }
+}
